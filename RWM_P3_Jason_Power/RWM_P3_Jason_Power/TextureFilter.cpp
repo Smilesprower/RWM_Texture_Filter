e121fbca4381da16ad3f4898a30bffb9317fecc2
Smilesprower <c00185326@itcarlow.ie>
2016-12-13 13:55:45 +0000

    Change ignore file

--- RWM_P3_Jason_Power/RWM_P3_Jason_Power/TextureFilter.cpp
+++ RWM_P3_Jason_Power/RWM_P3_Jason_Power/TextureFilter.cpp
@@ -35,27 +35,25 @@ SDL_Rect TextureFilter::getDestRect(int id)
 void TextureFilter::greyEverything(int id)
 {
 	SDL_Texture* tempTex = m_textureMap[id]->getTexture();
-	int tempPitch = 0;
-	void* tempPixels = NULL;
+	SDL_Surface * tempSurf = m_textureMap[id]->getSurface();
+	SDL_Rect * tempRect = &m_textureMap[id]->getDestRect();
+	Uint32* tempPixels = (Uint32*)m_textureMap[id]->getPixels();
 
-	SDL_LockTexture(tempTex, NULL, &tempPixels, &tempPitch);
-
-	Uint32* pixels = (Uint32*)tempPixels;
-
-	int pixelCount = (m_textureMap[id]->getSurface()->pitch * 0.25f) *  m_textureMap[id]->getSurface()->h;
+	int pixelCount = (tempSurf->pitch * 0.25f) *  tempSurf->h;
 	for (int i = 0; i < pixelCount; ++i)
 	{
-		Uint8 a = pixels[i] >> 24 & 0xFF;
+		Uint8 a = tempPixels[i] >> 24 & 0xFF;
 		if (a != 0)
 		{
-			Uint8 r = pixels[i] >> 16 & 0xFF;
-			Uint8 g = pixels[i] >> 8 & 0xFF;
-			Uint8 b = pixels[i] & 0xFF;
+			Uint8 r = tempPixels[i] >> 16 & 0xFF;
+			Uint8 g = tempPixels[i] >> 8 & 0xFF;
+			Uint8 b = tempPixels[i] & 0xFF;
 			Uint8 v = 0.212671f * r + 0.715160f * g + 0.072169f * b;
-			pixels[i] = (0xFF << 24) | (v << 16) | (v << 8) | v;
+			tempPixels[i] = (0xFF << 24) | (v << 16) | (v << 8) | v;
 		}
 	}
-	SDL_UnlockTexture(tempTex);
+	SDL_UpdateTexture(tempTex, tempRect, tempPixels, tempSurf->pitch);
+
 	// Add a flag for transparency for the user
 	SDL_SetTextureBlendMode(tempTex, SDL_BLENDMODE_BLEND);
 }
@@ -64,9 +62,9 @@ void TextureFilter::resetPixels(int id)
 {
 	SDL_Texture* tempTex = m_textureMap[id]->getTexture();
 	SDL_Surface * tempSurf = m_textureMap[id]->getSurface();
-	int pitch = 0;
-	void* tempPixels = NULL;
-	SDL_LockTexture(tempTex, NULL, &tempPixels, &pitch);
-	memcpy(tempPixels, tempSurf->pixels, tempSurf->pitch * tempSurf->h);
-	SDL_UnlockTexture(tempTex);
+	SDL_Rect * tempRect = &m_textureMap[id]->getDestRect();
+	void* tempPixels = tempSurf->pixels;
+
+	SDL_UpdateTexture(tempTex, tempRect, tempPixels, tempSurf->pitch);
+
 }
